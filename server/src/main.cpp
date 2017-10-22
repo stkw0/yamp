ef64b26eb9fb5128fddf49e7c13942a830e18c3c
David Roman <davidroman96@gmail.com>
2017-10-22 12:11:19 +0200

    cmake: Add clang-format and spdlog

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include <spdlog/spdlog.h>
+
 #include <grpc++/grpc++.h>
 
 #include "yamp.grpc.pb.h"
@@ -9,34 +11,36 @@ using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using yamp::HelloRequest;
 using yamp::HelloReply;
+using yamp::HelloRequest;
 
 class GreeterServiceImpl final : public yamp::Greeter::Service {
-   Status SayHello(ServerContext* context, const HelloRequest* request, HelloReply* reply) override {
-      std::string prefix("Hello ");
-      std::cout << request->name() << std::endl;
-      reply->set_message(prefix + request->name());
-      return Status::OK;
-   }
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+                    HelloReply* reply) override {
+        std::string prefix("Hello ");
+        std::cout << request->name() << std::endl;
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
 };
 
 void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
 
-  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
 
 int main() {
-   RunServer();
+    auto logging = spdlog::stdout_color_mt("global");
+    RunServer();
 }
